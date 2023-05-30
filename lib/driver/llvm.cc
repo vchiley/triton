25cecaaff1207c5991eb435ebc793456df9dd481
Vitaliy Chiley <vitaliyachiley@gmail.com>
2023-05-30 09:49:45 -0700

    updt sm_90

--- lib/driver/llvm.cc
+++ lib/driver/llvm.cc
@@ -163,8 +163,8 @@ namespace triton
     std::string llir_to_ptx(llvm::Module *module, int cc, int version)
     {
       // LLVM version in use may not officially support target hardware
-      int max_nvvm_cc = 75;
-      int max_nvvm_ptx = 74;
+      int max_nvvm_cc = 90;
+      int max_nvvm_ptx = 80;
       // options
       auto options = llvm::cl::getRegisteredOptions();
       auto *short_ptr = static_cast<llvm::cl::opt<bool> *>(options["nvptx-short-ptr"]);
